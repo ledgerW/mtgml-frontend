2698c53567ff6223703c93f71ac5681aed767025
Ledger West <lwest@mosaicint.local>
2019-11-10 17:19:11 -0500

    feat: React frontend setup

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "mtgml"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://wa4fum0mg7.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_wmyRRMMHK",
+    APP_CLIENT_ID: "6ndt0ouekm6vm3n1tvf8hl380k",
+    IDENTITY_POOL_ID: "us-east-1:c586e093-4798-407d-b418-24dffc309022"
+  }
+};
---
+++ src/containers/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>MTGML</h1>
+        <p>Understand your deck.</p>
+        <p>Make it better.</p>
+      </div>
+    </div>
+  );
+}
