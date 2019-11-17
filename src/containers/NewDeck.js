e1b10c6eb308f1ceab02266f65e8da7fafc38b45
Ledger West <lwest@mosaicint.local>
2019-11-17 00:32:04 -0500

    feat: add create deck page and save to Dynamo and S3

---
+++ src/containers/NewDeck.js
@@ -0,0 +1,81 @@
+import React, { useRef, useState } from "react";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { API } from "aws-amplify";
+import LoaderButton from "../components/LoaderButton";
+import config from "../config";
+import "./NewDeck.css";
+import { s3Upload } from "../libs/awsLib";
+
+export default function NewDeck(props) {
+  const file = useRef(null);
+  const [content, setContent] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function validateForm() {
+    return content.length > 0;
+  }
+
+  function handleFileChange(event) {
+    file.current = event.target.files[0];
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
+      alert(
+        `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
+          1000000} MB.`
+      );
+      return;
+    }
+
+    setIsLoading(true);
+
+    try {
+      const attachment = file.current
+        ? await s3Upload(file.current)
+        : null;
+
+      await createDeck({ content, attachment });
+      props.history.push("/");
+    } catch (e) {
+      alert(e);
+      setIsLoading(false);
+    }
+  }
+
+  function createDeck(deck) {
+    return API.post("decks", "/decks", {
+      body: deck
+    });
+  }
+
+  return (
+    <div className="NewDeck">
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="content">
+          <FormControl
+            value={content}
+            componentClass="textarea"
+            onChange={e => setContent(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="file">
+          <ControlLabel>Attachment</ControlLabel>
+          <FormControl onChange={handleFileChange} type="file" />
+        </FormGroup>
+        <LoaderButton
+          block
+          type="submit"
+          bsSize="large"
+          bsStyle="primary"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
+          Create
+        </LoaderButton>
+      </form>
+    </div>
+  );
+}
