3cae59438b888e3067dd58b9364ceb6879356917
Ledger West <lwest@mosaicint.local>
2019-11-24 13:57:44 -0500

    chore: config and build scripts for netlify

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   STRIPE_KEY: "pk_test_Y0iTsszHSlEHYP6gGfxsRUwX00S1UEqgpK",
   s3: {
     REGION: "us-east-1",
@@ -15,4 +14,33 @@ export default {
     APP_CLIENT_ID: "4b6fbfmc13937cp8vh8blus5uc",
     IDENTITY_POOL_ID: "us-east-1:c2d4e3f8-fb29-4a13-9e8b-a88c7f1f2d04"
   },
+};
+
+const prod = {
+  STRIPE_KEY: "pk_test_Y0iTsszHSlEHYP6gGfxsRUwX00S1UEqgpK",
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "mtgml-prod-attachmentsbucket-pdcw0jea0tga"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://4o28vvlwg9.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_Ka9TkZJ1O",
+    APP_CLIENT_ID: "29erkh67kqeb2nvmvvj6c1v2rn",
+    IDENTITY_POOL_ID: "us-east-1:fc5f117b-b733-472f-b9ff-4e35686ec3bd"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
