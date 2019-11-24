6f39bcd0953cf2b900471909abb669703660857d
Ledger West <lwest@mosaicint.local>
2019-11-23 20:31:58 -0500

    config: update

--- src/config.js
+++ src/config.js
@@ -3,16 +3,16 @@ export default {
   STRIPE_KEY: "pk_test_Y0iTsszHSlEHYP6gGfxsRUwX00S1UEqgpK",
   s3: {
     REGION: "us-east-1",
-    BUCKET: "mtgml"
+    BUCKET: "mtgml-dev-attachmentsbucket-1x9s74kqwhnlh"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://wa4fum0mg7.execute-api.us-east-1.amazonaws.com/prod"
+    URL: "https://otspimlra4.execute-api.us-east-1.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_wmyRRMMHK",
-    APP_CLIENT_ID: "6ndt0ouekm6vm3n1tvf8hl380k",
-    IDENTITY_POOL_ID: "us-east-1:c586e093-4798-407d-b418-24dffc309022"
+    USER_POOL_ID: "us-east-1_fdBuXli5D",
+    APP_CLIENT_ID: "4b6fbfmc13937cp8vh8blus5uc",
+    IDENTITY_POOL_ID: "us-east-1:c2d4e3f8-fb29-4a13-9e8b-a88c7f1f2d04"
   },
 };
